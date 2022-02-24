d08bee61ae46a1c2273d0baffa6541f79f7045ab
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-02-24 12:38:30 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector('#check').addEventListener('click', check)
+
+
+function check() {
+    const day = document.querySelector('#day').value.toLowerCase()
+
+    if(day === "tuesday" || day === "thursday") {
+        console.log("YOU HAVE CLASS") 
+     } else if( day === "saturday" || day === "sunday") {
+         console.log("Its The Weekend!!🍕")
+     } else {
+         console.log("BORINGGG")
+     }
+        
+    }
